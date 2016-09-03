0f919dfd00f8bf1fd508387c91d4126d3fcd9b1c
sbocicot <stevebocicot@icloud.com>
2016-09-03 15:07:31 -0400

    "modification de la page d'acceuil"

--- defines.php
+++ defines.php
@@ -1,9 +1,16 @@
 <?php
-define('SITE_NAME', 'Agence DreamSky');
+define('SITE_NAME', 'Saf Saf Agengece');
 define('HOME_PAGE_NAME', 'Accueil');
 
 $menu_data = array(
     'Accueil' => 'index.php',
     'Contact' => 'contact.php',
     'Catalogue' => 'catalog.php',
+);
+$top_menu = array(
+  'Nos service' => '#',
+    '514 444-22-33' => '#',
+    'A Propos' => '#',
+    '<span class="glyphicon glyphicon-user"></span> Log in' => '#',
+
 );
--- index.php
+++ index.php
@@ -1,9 +1,37 @@
 <?php
-	require_once('views/page_top.php');// Inclusion des defines
+require_once(dirname(__FILE__) . '/./defines.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<header>
+    <img src="image/logoSafsaf.jpg" alt="logo">
+</header>
+<ul class="pull-right">
+    <?php foreach ($top_menu as $label => $filename) { ?>
+        <li>
+            <a href="<?= $filename ?>"><?= $label ?></a>
+        </li>
+    <?php } ?>
+</ul>
+<?php
+require_once('views/page_top.php');// Inclusion des defines
 ?>
 <main>
     <h1>Accueil</h1>
 </main>
+
+
+</div>
 <?php
-	require_once('views/page_bottom.php');// Inclusion des defines
+require_once('views/page_bottom.php');// Inclusion des defines
 ?>
+</body>
